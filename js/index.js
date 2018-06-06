3771a480d8a688a8d7309125121c6b707c4eb720
Dominique Rochefort <drochefort@tribalnova.com>
2018-06-06 08:58:30 -0400

    clean up console log

--- js/index.js
+++ js/index.js
@@ -1,8 +1,5 @@
-console.log('logging something');
-
 function onButtonClick(evt) {
     console.log('I was clicked', evt.target.id);
 }
-
 
 document.getElementById('add').addEventListener('click', onButtonClick);
